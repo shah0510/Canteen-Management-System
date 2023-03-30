ad473b141b34d453b7a9dcd794c23eeb2ffbb080
shah0510 <109919237+shah0510@users.noreply.github.com>
2023-03-31 03:17:01 +0530

    Update urls.py

--- Restaurant_management_system/urls.py
+++ Restaurant_management_system/urls.py
@@ -19,11 +19,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.contrib.auth import views as auth_views
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index,  name='index'),
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
